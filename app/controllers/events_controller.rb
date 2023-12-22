c12b8df29d73a5b3d99106b3adce951b1068ade7
odilsonjs <italisodilsonwoodly@hotmail.com>
2023-12-22 09:52:09 -0500

    Update EventsController and routes file

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,6 @@
 class EventsController < ApplicationController
+    before_action :authenticate_user!, except: [:index]
+
     def index
         @events = Event.all
     end
@@ -8,5 +10,17 @@ class EventsController < ApplicationController
     end
 
     def create
+        @event = current_user.build_event(event_params)
+        if @event.save
+            redirect_to events_path, notice: "Event was successfully created!"
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def event_params
+        params.require(:event).permit(:title, :date, :location)
     end
 end
