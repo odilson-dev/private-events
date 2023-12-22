9db1d487eaf3bcd2dc3acc1894afff908218e96d
odilsonjs <italisodilsonwoodly@hotmail.com>
2023-12-22 11:12:47 -0500

    Build the form for creating a event

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,7 @@ class Event < ApplicationRecord
     validates :title, presence: true
     validates :date, presence: true
     validates :location, presence: true
+    validates :description, presence: true
 
     belongs_to :creator, class_name: "User"
 end
