04e083577456ece91581c308c988af3ff05bccdd
odilsonjs <italisodilsonwoodly@hotmail.com>
2023-12-22 07:54:20 -0500

    Add validations to User and Event models

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,11 @@ class EventsController < ApplicationController
     def index
         @events = Event.all
     end
+
+    def new
+        @event = Event.new
+    end
+
+    def create
+    end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ApplicationRecord
+    
+    validates :title, presence: true
+    validates :date, presence: true
+    validates :location presence: true
+
     belongs_to :creator, class_name: "User"
 end
