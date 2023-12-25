c85ec3815b92416905d6de08123781bfe84a6fbf
odilsonjs <italisodilsonwoodly@hotmail.com>
2023-12-25 12:46:06 -0500

    Add creator_id to events and migrate
    Using: rails generate migration AddCreatorIdToEvents creator_id:integer
    and rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_22_200031) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_25_174445) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_22_200031) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id"
   end
 
   create_table "users", force: :cascade do |t|
