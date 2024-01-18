d505756255e5e4268ee2af6ef74efa506fa76cce
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-01-18 11:41:53 -0500

    Set up the mailer for users signed up

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+    default from: 'no-reply@example.com'
+
+  def welcome_email
+    @user = params[:user]
+    @url  = 'http://localhost:3000/login'
+    mail(to: @user.email, subject: 'Welcome to My Awesome WebSite powered by Rails')
+  end
 end
